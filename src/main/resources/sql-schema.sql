6151788300ab7046ada1ca3e5b687fc93133e972
CGuthrieQA <CGuthrie@QA.com>
2021-01-22 09:44:41 +0000

    W5P-26 added sql schema for orders table

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -5,10 +5,15 @@ CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
-  
+);
+
+CREATE TABLE IF NOT EXISTS `ims` . `orders` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`fk_customers_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`fk_customers_id`) REFERENCES customers(`id`)
 );
