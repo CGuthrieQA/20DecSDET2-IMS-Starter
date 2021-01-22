a75b24b4abfe34d08f9c19ced08fc07a39ac4e71
CGuthrieQA <CGuthrie@QA.com>
2021-01-22 10:27:12 +0000

    W5P-13 getting used to commit naming is hard, also added CREATE method to order controller

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -1,5 +1,49 @@
 package com.qa.ims.controller;
 
-public class OrderController {
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderDao;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.JavaUtilities;
+
+public class OrderController implements ICrudController<Order> {
+	
+	// setup logger
+	 public static final Logger LOGGER = LogManager.getLogger();
+	 
+	 // declare variables
+    private OrderDao OrderDao;
+    private JavaUtilities javaUtilities;
+
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// CREATE
+	@Override
+	public Order create() {
+        LOGGER.info("Please enter id of customer ordering");
+        Long fk_customers_id = javaUtilities.getLong();
+        Order order = OrderDao.create(new Order(fk_customers_id));
+        LOGGER.info("Customer created");
+        return order;
+	}
+
+	@Override
+	public Order update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
