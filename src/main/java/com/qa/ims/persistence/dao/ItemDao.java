584263896aa0f9a16a80147ed9649d7755097a6f
CGuthrieQA <CGuthrie@QA.com>
2021-01-22 15:08:04 +0000

    W5P-10 fixed some issues in item dao

--- src/main/java/com/qa/ims/persistence/dao/ItemDao.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDao.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -12,62 +13,77 @@ import org.apache.logging.log4j.Logger;
 
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.DatabaseUtilities;
-import com.qa.ims.utils.JavaUtilities;
 
 public class ItemDao implements IDomainDao<Item> {
+	
 	// setup logger
 	public static final Logger LOGGER = LogManager.getLogger();
-	
-	// declare variables
-    private ItemDao itemDao;
-    private JavaUtilities javaUtilities;
-	
-	
+
 	// CRUD
-	
+
 	// CREATE
 	@Override
 	public Item create(Item item) {
 		try (Connection connection = DatabaseUtilities.getInstance().getConnection();
-                PreparedStatement statement = connection
-                        .prepareStatement("INSERT INTO items(name, value) VALUES (?, ?)");) {
-            statement.setString(1, item.getName());
-            statement.setDouble(2, item.getValue());
-            statement.executeUpdate();
-            return readLatest();
-        } catch (Exception e) {
-            LOGGER.debug(e);
-            LOGGER.error(e.getMessage());
-        }
-        return null;
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO items(name, value) VALUES (?, ?)");) {
+			statement.setString(1, item.getName());
+			statement.setDouble(2, item.getValue());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
 	}
-	
-	
-	// READ
-	@Override
-	 public List<Item> readAll() {
-        List<Item> items = itemDao.readAll();
-        for (Item item : items) {
-            LOGGER.info(item);
-        }
-        return items;
-    }
 
-	
+	// READ
+	public Item read(Long id) {
+		try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM orders WHERE id = ?");) {
+			statement.setLong(1, id);
+			ResultSet resultSet = statement.executeQuery();
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	// READ all
+	@Override
+	public List<Item> readAll() {
+		try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");) {
+			List<Item> items = new ArrayList<>();
+			while (resultSet.next()) {
+				items.add(modelFromResultSet(resultSet));
+			}
+			return items;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
 	// READ latest
 	public Item readLatest() {
-        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
-            resultSet.next();
-            return modelFromResultSet(resultSet);
-        } catch (Exception e) {
-            LOGGER.debug(e);
-            LOGGER.error(e.getMessage());
-        }
-        return null;
-    }
-
+		try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
 
 	@Override
 	public Item update(Item t) {
