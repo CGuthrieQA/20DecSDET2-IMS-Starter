463960f6d038eb5270bae6cf2dcbbcd299e496e5
CGuthrieQA <CGuthrie@QA.com>
2021-01-27 09:28:35 +0000

    W5P-41 added dummy data to sql db in test branch

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,4 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `orders` (`fk_customers_id`) VALUES ('1');
+INSERT INTO `items` (`name`,`value`) VALUES ('bread', '0.58');
+INSERT INTO `orders_items` (`fk_orders_id`,`fk_items_id`) VALUES ('1', '1');
